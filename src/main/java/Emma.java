d948d7490e3144eba7c4103fd4b7a68b1fb03490
MetalPower26 <matthewa.2606@gmail.com>
2026-08-19 21:13:20 +0800

    Rename chatbot to Emma

---
+++ src/main/java/Emma.java
@@ -0,0 +1,10 @@
+public class Emma {
+    public static void main(String[] args) {
+        String banner = " _____\n"
+                + "| ____|  _ __ ___   _ __ ___    __ _ \n"
+                + "|  _|   | '_ ` _ \\  | '_ ` _ \\   / _` |\n"
+                + "| |___  | | | | | | | | | | | | | (_| |\n"
+                + "|_____| |_| |_| |_| |_| |_| |_|  \\__,_|\n";
+        System.out.println(banner);
+    }
+}
